f02b3ff43d4777989dbcfc44c616d45b75e58d60
S-Shimotori <gingerale.jagajaga@gmail.com>
2025-11-26 20:37:15 +0900

    Fix swift-format warning

--- Tests/PackageManifestKitTests/TargetTests.swift
+++ Tests/PackageManifestKitTests/TargetTests.swift
@@ -10,13 +10,14 @@ struct TargetTests {
     @Test
     func decodeWarningControlSetting() throws {
         // .disableWarning("conversion")
-        let rawJSONString = """
-        {
-            "disableWarning" : {
-              "_0" : "conversion"
+        let rawJSONString =
+            """
+            {
+                "disableWarning" : {
+                    "_0" : "conversion"
+                }
             }
-        }
-        """
+            """
 
         let target = try jsonDecoder.decode(
             Target.TargetBuildSetting.Kind.self,
